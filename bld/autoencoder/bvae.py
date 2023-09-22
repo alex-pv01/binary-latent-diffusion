8dcfbafb9cbbcb3422168ac41aa70bcc6cb5fba2
alex <alex-pv01@proton.me>
2023-09-22 12:44:48 +0000

    preparing main

--- bld/autoencoder/bvae.py
+++ bld/autoencoder/bvae.py
@@ -1,14 +1,8 @@
-import sys
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 from torchvision import transforms
-
-import matplotlib.pyplot as plt
-
-from datasets import load_dataset
 
 from bld.modules.modules import Encoder, Decoder, BinaryQuantizer
 from bld.modules.losses import TVLoss, VGGLoss, WeightedLoss
@@ -99,57 +93,3 @@ def train_b_vae(bvae, dataset, num_epochs, lr, resolution = 32):
             opt.step()
         print("Epoch ", epoch)
 
-def plot_tensor_image(tensor):
-    # Convert the tensor to a NumPy array
-    image_array = tensor.squeeze(0).permute(1, 2, 0).detach().cpu().numpy()
-    
-    # Ensure values are in the range [0, 1] (assuming your tensor values are normalized)
-    image_array = image_array.clip(0, 1)
-    
-    # Display the image
-    plt.imshow(image_array)
-    plt.axis('off')  # Turn off the axis labels and ticks
-    plt.show()
-
-
-def main():
-    # Instantiate and train the B-VAE
-    dataset  = load_dataset("beans", split="train")[:500]['image']
-
-    # Check if a GPU is available
-    if torch.cuda.is_available():
-        # Set the device to the first available GPU
-        device = torch.device("cuda")
-        print("GPU is available.")
-    else:
-        # If no GPU is available, use the CPU
-        device = torch.device("cpu")
-        print("GPU is not available, using CPU.")
-
-    resolution = 128
-
-    model = BVAEModel(device, resolution)
-    num_epochs = 20
-    learning_rate = 1e-3
-
-    train_b_vae(model, dataset, num_epochs, learning_rate, resolution)
-
-    image = dataset[0]
-
-    trans = transforms.ToTensor()
-
-    img = image.copy()
-    img = img.resize((resolution,resolution))
-
-    tensor_image = trans(img).unsqueeze(0).to(device)
-
-    plot_tensor_image(tensor_image)
-
-    r_tensor_image = model.forward(tensor_image) 
-
-    plot_tensor_image(r_tensor_image)
-
-    return 0
-
-if __name__== '__main__':
-    sys.exit(main())
